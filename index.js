eafea0ad39aebc36f90c67454ebfa014335db0f4
Aman Kumar <amantheroot@gmail.com>
2019-04-22 12:17:06 +0530

    organized

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ require('electron-reload')(__dirname);
 
 app.on('ready', () =>{
   let win = new BrowserWindow({height: 600, width: 800});
-  win.loadURL(`file://${__dirname}/index.html`);
+  win.loadURL(`file://${__dirname}/app/index.html`);
 });
 
 exports.openWindow = (filename) => {
   let win = new BrowserWindow({height: 600, width: 800});
-  win.loadURL(`file://${__dirname}/${filename}.html`);
+  win.loadURL(`file://${__dirname}/app/${filename}.html`);
 };
